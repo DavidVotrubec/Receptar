23f4d2800bc1c99157462a36d0ac276abb3270de
David Votrubec <votrubec.d@st-software.com>
2014-07-29 19:28:11 +0200

    Basic draft of filter

---
+++ Scripts/recipesApp/RecipesApp.js
@@ -0,0 +1,16 @@
+﻿//TODO: var Module = angular.module("recipesApp", ["ngRoute", "ngResource", "STAngular", "$strap", "localytics.directives"]);
+
+var Module = angular.module("RecipesApp", ["ngRoute", "ngResource"])
+.config([
+        "$routeProvider",
+        "$locationProvider",
+        "$httpProvider",
+        function ($routeProvider, $locationProvider, $httpProvider) {
+
+            $locationProvider.html5Mode(true);
+
+            $routeProvider.when("/", { templateUrl: "recipes-list.html", controller: "RecipesListCtrl", controllerAs: 'vm', caseInsensitiveMatch: true });
+
+            $routeProvider.otherwise({ redirectTo: "/"});
+        }
+    ]);
---
+++ Scripts/recipesApp/controllers/RecipesListCtrl.js
@@ -0,0 +1,63 @@
+﻿angular.module("RecipesApp").controller("RecipesListCtrl", ["$http", function ($http) {
+
+    //TODO: Move to some config
+    var baseUrl = 'http://davidvotrubec.apiary-mock.com/';
+
+    var that = this;
+    this.isLoadingIngredients = false;
+    this.isLoadingRecipes = false;
+    this.ingredients = [];
+    this.recipes = [];
+    this.filter = {ingredientIds: []};
+
+    //#region Ingredients
+    function loadIngedients() {
+        that.isLoadingIngredients = true;
+        $http.get(baseUrl + 'ingedients')
+            .success(ingredientsLoaded)
+            .error(function() {
+                that.isLoadingIngredients = false;
+        });
+    }
+
+    function ingredientsLoaded(data) {
+        that.isLoadingIngredients = false;
+        that.ingredients = data.items;
+    }
+    //#endregion
+
+    //#region Recipes
+    function loadRecipes() {
+        that.isLoadingRecipes = true;
+        $http.get(baseUrl + 'recipes')
+            .success(recipesLoaded)
+            .error(function () {
+                that.isLoadingRecipes = false;
+            });
+    }
+
+    function recipesLoaded(data) {
+        that.isLoadingRecipes = false;
+        that.recipes = data.items;
+    }
+
+    function filterRecipes(ingredientIds) {
+        
+    }
+    //#endregion Recipes
+
+    this.filterBy = function(item) {
+        var isItemUsed = _.indexOf(that.filter.ingredientIds, item.Id) > -1;
+
+        if (isItemUsed) {
+            that.filter.ingredientIds = _.without(that.filter.ingredientIds, item.Id);
+            item.isUsed = false;
+        } else {
+            that.filter.ingredientIds.push(item.Id);
+            item.isUsed = true;
+        }
+    }
+
+    loadIngedients();
+    loadRecipes();
+}])
